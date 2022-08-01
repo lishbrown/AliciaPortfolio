5402bf09e6861e1810bd4b7c9d852113a41aecad
lishbrown <65086272+lishbrown@users.noreply.github.com>
2022-08-01 08:41:39 -0600

    Adding if statements and fixing links to pages

--- script.js
+++ script.js
@@ -8,11 +8,39 @@ let mY = 0;
 const z = 50;
 const i = 10;
 
+// Main Button
+
 const animateCircles = (e,x,y) => {
+        if(!mainImg) return;
+
         if(x < mX) {
-        circles.forEach((circle) => {
-            circle.style.left = `-${z}px`;
+            circles.forEach((circle) => {
+                circle.style.left = `-${z}px`;
         });
+        document.body.addEventListener("mousemove", (e) => {
+        let ripple;
+
+        if (mainBtn) {
+            mainBtn.addEventListener("mouseenter", (e) => {
+            const left = e.clientX - e.target.getBoundingClientRect
+            ().left;
+            const top = e.clientY - e.target/getBoundingClientRect
+            ().top;
+
+            ripple = document.createElement('div');
+            ripple.classList.add("ripple");
+            ripple.style.left = `${left}px`;
+            ripple.style.top = `${top}px`
+            mainBtn.prepend(ripple);
+            });
+
+            mainBtn.addEventListener.createElement('div');
+            mainBtn.removeChild(ripple);
+        }
+    })
+
+    // End of Main Button
+
         mainImg.style.left = `-${z}px`;
     } else if(x > mX) {
         circles.forEach((circle) => {
@@ -45,29 +73,7 @@ document.body.addEventListener("mousemove", (e) => {
     animateCircles(e,x,y);
     });
 
-    // Main Button
-    const mainBtn = document.querySelector('.main-btn');
-
-    let ripple;
-
-    mainBtn.addEventListener("mouseenter", (e) => {
-        const left = e.clientX - e.target.getBoundingClientRect
-        ().left;
-        const top = e.clientY - e.target.getBoundingClientRect
-        ().top;
-       
-        ripple = document.createElement('div');
-        ripple.classList.add("ripple");
-        ripple.style.left = `${left}px`;
-        ripple.style.top = `${top}px`;
-        mainBtn.prepend(ripple);
-    });
-
-    mainBtn.addEventListener("mouseleave", () => {
-        mainBtn.removeChild(ripple);    
-    })
-    // End of Main Button
-
+    
      // Navigation
      const menuIcon = document.querySelector(".menu-icon");
      const navbar = document.querySelector(".navbar");
@@ -86,21 +92,32 @@ document.body.addEventListener("mousemove", (e) => {
         menuIcon.classList.remove("show-menu-icon");
         navbar.classList.remove("hide-navbar");
      });
+
+     document.body.addEventListener("mousemove", (e) => {
+        document.querySelectorAll(".service-btn").forEach(
+            (service) => {
+                service.addEventListener("click", e => {
+                    e.preventDefault();
+                });
+            }
+        )
+     })
       // End of Navigation
 
     // Section 2
-    document.querySelectorAll(".service-btn").forEach(
+      document.querySelectorAll(".service-btn").forEach(
         (service) => {
-            service.addEventListener("click", e => {
+            service.addEventListener("click", (e) => {
                 e.preventDefault();
-            
+
                 const serviceText = service.nextElementSibling;
                 serviceText.classList.toggle("change");
 
-                const rightPosition = serviceText.classList.contains("change") ? `calc(100% - ${getComputedStyle(service.firstElementChild).width})`
+                const rightPosition = serviceText.classList.contains("change") ? `calc(100% - ${getComputedStyle (service.firstElementChild).width})`
                 :0;
 
                 service.firstElementChild.style.right = rightPosition;
+
             });
         });
     // End of Section 2
@@ -110,7 +127,7 @@ document.body.addEventListener("mousemove", (e) => {
     const username = document.getElementById("name");
     const email = document.getElementById("email");
     const subject = document.getElementById("subject");
-    const messages = document.querySelectorAll(".message");
+    const message = document.querySelectorAll(".message");
 
     const error = (input, message) => {
         input.nextElementSibling.classList.add("error");
@@ -131,11 +148,13 @@ document.body.addEventListener("mousemove", (e) => {
         });
     };
 
-    form.addEventListener("submit", e => {
-        e.preventDefault();
+    if (form) {
+        form.addEventListener("submit", e => {
+            e.preventDefault();
 
-        checkRequiredFields([username, email, subject, message]);
-    });
+            checkRequiredFields([username, email, subject, message]);
+        });
+    }
     // End of Form Validation
 
     
